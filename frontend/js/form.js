d701167715953aa88c66229ca41cedc18afe91ec
Ala111205 <sadham070403@gmail.com>
2025-12-13 09:45:16 +0530

    Online

--- frontend/js/form.js
+++ frontend/js/form.js
@@ -23,7 +23,7 @@ if (contactForm) {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/contact", {
+      const res = await fetch("https://personal-portfolio-6qmz.onrender.com/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, subject, message })
